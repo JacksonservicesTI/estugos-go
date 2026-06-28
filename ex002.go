9b6acffebee069f799d6c0dcd830b427e83e8fd4
JacksonservicesTI <152933663+JacksonservicesTI@users.noreply.github.com>
2026-06-28 12:11:28 -0300

    Create ex002.go

---
+++ ex002.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+
+//
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 {
+			fmt.Println("Pin")
+		} else if i%5 == 0 {
+			fmt.Println("Pan")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
